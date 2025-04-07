6d7e3be9aef47c038affb7ec5a40476610550378
Stefan <102488134+obradovicstefan@users.noreply.github.com>
2025-04-07 19:15:36 +0200

    added expandable images with modal for info

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const overlay = document.getElementById("modal-overlay");
 const modalTitle = document.getElementById("modal-title");
 const modalDescription = document.getElementById("modal-description");
 const modalImage = document.getElementById("modal-image");
+const awardItems = document.querySelectorAll(".award-content");
 
 function closeModal() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+  modal.classList.remove("modal-award");
 }
 
 document.querySelectorAll(".btn-open").forEach((button) => {
@@ -30,7 +32,30 @@ document.querySelectorAll(".btn-open").forEach((button) => {
   });
 });
 
+awardItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const title = item.getAttribute("data-title");
+    const description = item.getAttribute("data-description");
+    const image = item.getAttribute("data-image");
+    const imageClass = item.getAttribute("data-class");
+
+    modalTitle.textContent = title;
+    modalDescription.textContent = description;
+    modalImage.src = image;
+    modalImage.alt = title;
+
+    modal.classList.add("modal-award");
+
+    modalImage.className = "";
+    if (imageClass) {
+      modalImage.classList.add(imageClass);
+    }
+
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+});
+
 // Close modal logic
 document.querySelector(".btn-close").addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
-
